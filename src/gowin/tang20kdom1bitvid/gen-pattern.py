eaf35d8ee9a1a98ce0163d5ed40126332fc3721f
dominicbeesley <dominicbeesley@gmail.com>
2025-02-28 16:54:15 +0000

    longer patterns for more bits

--- src/gowin/tang20kdom1bitvid/gen-pattern.py
+++ src/gowin/tang20kdom1bitvid/gen-pattern.py
@@ -4,7 +4,7 @@ import random
 # constants
 bits = 5
 width = 10
-
+rep = 4
 
 # calculated constants
 max = 2**bits - 1
@@ -14,18 +14,21 @@ for i in range(max + 1):
     tgt = i / max
     acc = 0
     tot = random.randint(0, 1)
-    s = ""
-    for j in range(width):        
-        avg = tot / (j + 1)
-        if tgt > avg:
-            s = s + "1"
-            tot += 1
+    n = 1
+    for k in range(rep):
+        s = ""
+        for j in range(width):        
+            avg = tot / n
+            if tgt > avg:
+                s = s + "1"
+                tot += 1
+            else:
+                s = s + "0"
+            n += 1
+                
+        if i == max and k == rep-1:
+            print("when others => R := \"%s\";" % (s))
         else:
-            s = s + "0"
-            
-    if i == max:
-        print("when others => R := \"%s\";" % (s))
-    else:
-        print("when %d => R := \"%s\";" % (i, s))
+            print("when %d => R := \"%s\";" % (i * rep + k, s))
 
 
