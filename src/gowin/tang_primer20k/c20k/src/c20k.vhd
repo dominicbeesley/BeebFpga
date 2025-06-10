e616013ee27f515e095e4953ccef95fbf71b0483
dominicbeesley <dominicbeesley@gmail.com>
2025-06-11 00:53:44 +0100

    C20K  - use on board sdram - not working

--- src/gowin/tang_primer20k/c20k/src/c20k.vhd
+++ src/gowin/tang_primer20k/c20k/src/c20k.vhd
@@ -65,7 +65,6 @@ entity bbc_micro_tang20k is
         IncludeVideoNuLA   : boolean := true;
         IncludeTrace       : boolean := false;
         IncludeHDMI        : boolean := true;
-        IncludeBootStrap   : boolean := false;
         IncludeMonitor     : boolean := true;
         IncludeCoPro6502       : boolean := false;
         IncludeSoftLEDs        : boolean := true;
@@ -118,7 +117,36 @@ entity bbc_micro_tang20k is
 
         -- 1 bit DAC audio out
         audiol_o        : out   std_logic;
-        audior_o        : out   std_logic
+        audior_o        : out   std_logic;
+
+
+        -- C20K motherboard SRAM
+
+        -- TODO: ensure CPU is suspended and not clashing on D/A bus        
+        mem_A_o             : out std_logic_vector(20 downto 0);
+        mem_D_io            : inout std_logic_vector(7 downto 0);
+        mem_nCE_FL_o        : out   std_logic;
+        mem_nCE_BB_o        : out   std_logic;
+        mem_nCE_o           : out   std_logic_vector(3 downto 1);
+        mem_nOE_o           : out   std_logic;
+        mem_nWE_o           : out   std_logic;
+
+      -- C20K multiplexer
+
+        -- mux clock outputs
+        p_1MHZ_E_o          : out    std_logic;
+        p_2MHZ_E_o          : out    std_logic;
+
+        -- mux control outputs
+        mux_nALE_o          : out    std_logic;
+        mux_D_nOE_o         : out    std_logic;
+        mux_I0_nOE_o        : out    std_logic;
+        mux_I1_nOE_o        : out    std_logic;
+        mux_O0_nOE_o        : out    std_logic;
+        mux_O1_nOE_o        : out    std_logic;
+
+        -- mux multiplexed signals bus
+        mux_io              : inout  std_logic_vector(7 downto 0)
 
         );
 end entity;
@@ -344,10 +372,74 @@ function VOLUME_FN(log : in natural) return natural is
 
     signal dbg_keyboard_state : std_logic_vector(5 downto 0);
 
+    -- c20k stuff
+    signal ic_mhz1E_clken   : std_logic;
+    signal ic_mhz2E_clken   : std_logic;
+
+    signal i_SYS_A          : std_logic_vector(15 downto 0);
+    signal i_SYS_RnW        : std_logic;
+    signal i_sys_nRST       : std_logic;
+
+    signal ic_addr_ack_clken: std_logic;
+
+    signal i_sys_D_wr       : std_logic_vector(7 downto 0);
+
+    signal i_sys_D_rd       : std_logic_vector(7 downto 0);
+    signal i_sys_D_rd_clken : std_logic;
+
+    signal ipi_ser_cts      : std_logic;
+    signal ipi_ser_rx       : std_logic;
+    signal ipi_d_cas        : std_logic;
+    signal ipi_kb_nRST      : std_logic;
+    signal ipi_kb_CA2       : std_logic;
+    signal ipi_netint       : std_logic;
+    signal ipi_irq          : std_logic;
+    signal ipi_nmi          : std_logic;
+
+    signal ipi_j_i0         : std_logic;
+    signal ipi_j_i1         : std_logic;
+    signal ipi_j_spi_miso   : std_logic;
+    signal ipi_btn0         : std_logic;
+    signal ipi_btn1         : std_logic;
+    signal ipi_btn2         : std_logic;
+    signal ipi_btn3         : std_logic;
+    signal ipi_kb_pa7       : std_logic;
+
+    
+    signal ipo_SER_TX       : std_logic;
+    signal ipo_SER_RTS      : std_logic;
+
+    signal ipo_j_ds_nCS2    : std_logic;
+    signal ipo_j_ds_nCS1    : std_logic;
+    signal ipo_j_spi_clk    : std_logic;
+    signal ipo_VID_HS       : std_logic;
+    signal ipo_VID_VS       : std_logic;
+    signal ipo_VID_CS       : std_logic;
+    signal ipo_j_spi_mosi   : std_logic;
+    signal ipo_j_adc_nCS    : std_logic;
+
+
 begin
 
     -- C20K bodge
     btn2_n <= '1';
+
+    i_SYS_A          <= x"FFEA";
+    i_SYS_RnW        <= '1';
+    i_sys_nRST       <= '1';
+
+    ipo_SER_TX       <= '1';
+    ipo_SER_RTS      <= '1';
+    ipo_j_ds_nCS2    <= '1';
+    ipo_j_ds_nCS1    <= '1';
+    ipo_j_spi_clk    <= '1';
+    ipo_VID_HS       <= i_VGA_HS;
+    ipo_VID_VS       <= i_VGA_VS;
+    ipo_VID_CS       <= not (i_VGA_hs xor i_VGA_vs);
+    ipo_j_spi_mosi   <= '1';
+    ipo_j_adc_nCS    <= '1';
+
+
 
     --------------------------------------------------------
     -- BBC Micro Core
@@ -736,11 +828,10 @@ begin
     -- ??? Memory Controller
     --------------------------------------------------------
 
-    e_mem: entity work.mem_tang_primer_20k
+    e_mem: entity work.mem_tang_primer_c20k
         generic map (
             SIM => SIM,
             IncludeMonitor => IncludeMonitor,
-            IncludeBootStrap => IncludeBootStrap,
             IncludeMinimalBeeb => true,
             IncludeMinimalMaster => true,
             PRJ_ROOT => PRJ_ROOT,
@@ -761,6 +852,14 @@ begin
             core_nWE       => ext_nWE,
             core_nWE_long  => ext_nWE_long,
             core_nOE       => ext_nOE,
+
+            mem_A_o        => mem_A_o,
+            mem_D_io       => mem_D_io,
+            mem_nCE_FL_o   => mem_nCE_FL_o,
+            mem_nCE_BB_o   => mem_nCE_BB_o,
+            mem_nCE_o      => mem_nCE_o,
+            mem_nOE_o      => mem_nOE_o,
+            mem_nWE_o      => mem_nWE_o,
 
             led            => monitor_leds,
 
@@ -1018,5 +1117,90 @@ begin
         bitstream_o       => vid_b_o
    );
 
-   
+
+    --------------------------------------------------
+    -- C20K multiplexer
+    --------------------------------------------------
+
+    e_dut:entity work.c20k_peripherals_mux_ctl
+    generic map (
+        G_FAST_CLOCKSPEED    => 96000000,
+        G_BEEBFPGA           => true
+    )
+    port map (
+
+        -- clocks in   
+        clk_fast_i              => clock_96,
+
+        -- clock ens out in fast clock domain
+        mhz1E_clken_o           => ic_mhz1E_clken,
+        mhz2E_clken_o           => ic_mhz2E_clken,
+
+        -- state control in
+        reset_i                 => not hard_reset_n,
+
+        -- address and cycle selection from core
+        sys_cyc_en_i            => '1',
+        sys_A_i                 => i_SYS_A,
+        sys_RnW_i               => i_SYS_RnW,
+        sys_nRST_i              => i_sys_nRST,
+
+        -- address and cycle selection back to core
+        addr_ack_clken_o        => ic_addr_ack_clken,
+
+        sys_D_wr_i              => i_sys_D_wr,
+
+        -- data and inputs back from bus at end of cycle
+        sys_D_rd_o              => i_sys_D_rd,
+        sys_D_rd_clken_o        => i_sys_D_rd_clken,
+
+        -- mux clock outputs
+        mux_mhz1E_clk_o         => p_1MHZ_E_o,
+        mux_mhz2E_clk_o         => p_2MHZ_E_o,
+
+        -- mux control outputs
+        mux_nALE_o              => mux_nALE_o,
+        mux_D_nOE_o             => mux_D_nOE_o,
+        mux_I0_nOE_o            => mux_I0_nOE_o,
+        mux_I1_nOE_o            => mux_I1_nOE_o,
+        mux_O0_nOE_o            => mux_O0_nOE_o,
+        mux_O1_nOE_o            => mux_O1_nOE_o,
+
+        -- mux multiplexed signals bus   
+        mux_bus_io              => mux_io,
+
+        -- random other multiplexed pins out to FPGA (I0 phase)
+        p_ser_cts_o             => ipi_ser_cts,
+        p_ser_rx_o              => ipi_ser_rx,
+        p_d_cas_o               => ipi_d_cas,
+        p_kb_nRST_o             => ipi_kb_nRST,
+        p_kb_CA2_o              => ipi_kb_CA2,
+        p_netint_o              => ipi_netint,
+        p_irq_o                 => ipi_irq,
+        p_nmi_o                 => ipi_nmi,
+
+        -- random other multiplexed pins out to FPGA (I1 phase)
+        p_j_i0_o                => ipi_j_i0,
+        p_j_i1_o                => ipi_j_i1,
+        p_j_spi_miso_o          => ipi_j_spi_miso,
+        p_btn0_o                => ipi_btn0,
+        p_btn1_o                => ipi_btn1,
+        p_btn2_o                => ipi_btn2,
+        p_btn3_o                => ipi_btn3,
+        p_kb_pa7_o              => ipi_kb_pa7,
+
+        -- random other multiplexed pins in from FPGA (O0 phase)
+        p_SER_TX_i              => ipo_SER_TX,
+        p_SER_RTS_i             => ipo_SER_RTS,
+
+        -- random other multiplexed pins in from FPGA (O1 phase)
+        p_j_ds_nCS2_i           => ipo_j_ds_nCS2,
+        p_j_ds_nCS1_i           => ipo_j_ds_nCS1,
+        p_j_spi_clk_i           => ipo_j_spi_clk,
+        p_VID_HS_i              => ipo_VID_HS,
+        p_VID_VS_i              => ipo_VID_VS,
+        p_VID_CS_i              => ipo_VID_CS,
+        p_j_spi_mosi_i          => ipo_j_spi_mosi,
+        p_j_adc_nCS_i           => ipo_j_adc_nCS
+   );   
 end architecture;
